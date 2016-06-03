212fe9e3b934a6a6397d30f461506348d5336d38
Chris McKee <cmckee41@qub.ac.uk>
2016-06-03 17:49:03 +0100

    Moved fizzbuzz solution for cleanliness

--- euler1/src/main.rs
+++ euler1/src/main.rs
@@ -15,31 +15,4 @@ fn main(){
 
     println!("The answer is: {}", sum);
 
-    // FizzBuzz
-
-    for i in 0..100 {
-        let result = if (i % 3 == 0) | (i % 5 == 0) {
-            "FizzBuzz".to_string()
-        } else if i % 3 == 0 {
-            "Fizz".to_string()
-        } else if i % 5 == 0 {
-            "Buzz".to_string()
-        } else {
-            i.to_string()
-        };
-        println!("{}", result);
-
-    }
-
-    // Pattern matching fizzbuzz - found on SO but saved because it's pretty
-
-    for i in 1..101 {
-        match (i % 3, i % 5){
-            (0, 0) => println!("Fizzbuzz"),
-            (0, _) => println!("Fizz"),
-            (_, 0) => println!("buzz"),
-            _ => println!("{}", i),
-        }
-    }
-
 }
