7f033bb838e3ce0bb884ab58923ea05097817883
Chris McKee <cmckee41@qub.ac.uk>
2016-06-08 23:30:06 +0100

    Progress on text reader
    Need to fight the borrow checker / come up with a better way to
    store word data. File reading going as expected

--- io/src/text_reader.rs
+++ io/src/text_reader.rs
@@ -1,4 +1,7 @@
+use std::error::Error;
 use std::fs::File;
+use std::io::prelude::*;
+use std::path::Path;
 
 struct Counter<'a> {
     word: &'a str,
@@ -6,12 +9,31 @@ struct Counter<'a> {
 }
 
 pub fn read(path : &str) {
-    let mut words : Vec<&Counter> = vec![];
-    let mut f = try!(File::open(path));
-    let word_string = String::new();
-    try!(f.read_to_string(&mut word_string));
+    let mut words : Vec<Counter> = vec![];
+    let rpath = Path::new(path);
+    let display = rpath.display();
+
+    // totally ripped out of rust by example
+    let mut file = match File::open(&rpath) {
+            Err(why) => panic!("couldn't open {}: {}", display,
+                                                       why.description()),
+            Ok(file) => file,
+        };
+
+    let mut word_string = String::new();
+    file.read_to_string(&mut word_string);
     for word in word_string.split_whitespace() {
-        println!("{}", word);
+        let word_collected = words.iter()
+                             .find(|x| x.word == word);
+        match word_collected {
+            Some(x) => x.count += 1,
+            None => words.push(make_counter(word)),
+        }
     }
 
+}
+
+fn make_counter(word : &str) -> Counter {
+    let new_count = Counter { word:word , count : 1};
+    new_count
 }
